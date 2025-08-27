4f283b19542e070de8c2246d45bd112fe53def81
Github11200 <78944617+Github11200@users.noreply.github.com>
2025-08-27 16:27:35 -0700

    finished testing the motion profile

--- src/motions/trapezoidalMotionProfile.cpp
+++ src/motions/trapezoidalMotionProfile.cpp
@@ -3,27 +3,27 @@
 using namespace std;
 using namespace vex;
 
-Chassis::trapezoidalMotionProfile(double distance, MotionProfile motionProfileSettings, DriveParams driveParams, TurnParams turnParams, Settings settings)
+void Chassis::trapezoidalMotionProfile(double distance, MotionProfile motionProfileSettings, DriveParams driveParams, TurnParams turnParams, Settings settings)
 {
   // vf^2 = vi^2 + 2ad
   double maximumReachableVelocity = min(sqrt(2 * motionProfileSettings.maximumAcceleration * (distance / 2)), motionProfileSettings.maximumVelocity);
-
-  double cruiseDistance = distance - accelerationDistance - decelerationDistance;
-
-  // If there is no time to cruise then it's just a triangular profile
-  if (cruiseDistance < 0)
-    cruiseDistance = 0;
 
   double kV = 1 / maximumReachableVelocity;
 
   // vf = vi + at
   double accelerationTime = (maximumReachableVelocity / motionProfileSettings.maximumAcceleration);
-  double cruiseTime = (cruiseDistance / maximumReachableVelocity);
-  double decelerationTime = (motionProfileSettings.finalVelocity - maximumReachableVelocity) / motionProfileSettings.maximumAcceleration;
+  // TODO: Remove deceleration time
+  double decelerationTime = (maximumReachableVelocity) / motionProfileSettings.maximumAcceleration;
 
   // d = vf^2 / 2a
   double accelerationDistance = (motionProfileSettings.maximumAcceleration * pow(accelerationTime, 2)) / 2;
   double decelerationDistance = (motionProfileSettings.maximumAcceleration * pow(decelerationTime, 2)) / 2;
+  double cruiseDistance = distance - accelerationDistance - decelerationDistance;
+
+  // If there is no time to cruise then it's just a triangular profile
+  if (cruiseDistance < 0)
+    cruiseDistance = 0;
+  double cruiseTime = (cruiseDistance / maximumReachableVelocity);
 
   double totalTime = accelerationTime + decelerationTime + cruiseTime;
   double elapsedTime = 0;
@@ -34,20 +34,39 @@ Chassis::trapezoidalMotionProfile(double distance, MotionProfile motionProfileSe
   Pose<double> startPose = odometry->getPose();
   Pose<double> currentPose = startPose;
 
-  double setpoint, feedforward, driveError, turnError, driveOutput, turnOutput = 0;
+  double setpoint, setpointVelocity, setpointAcceleration, feedforward, driveError, turnError, driveOutput, turnOutput = 0;
+
+  // cout << "Cruise distance: " << cruiseDistance << endl;
+  cout << "max reachable velocity (not light speed): " << maximumReachableVelocity << endl;
+  cout << "acceleration distance: " << accelerationDistance << endl;
+  cout << "deceleration distance: " << decelerationDistance << endl;
+  // cout << "max acceleration: " << motionProfileSettings.maximumAcceleration << endl;
+  cout << "acceleration time: " << accelerationTime << endl;
+  cout << "deceleration time: " << decelerationTime << endl;
+  cout << "max velocity: " << maximumReachableVelocity << endl;
+  cout << "kv: " << kV << endl;
+  cout << "total time: " << totalTime << endl;
 
   while (elapsedTime < totalTime)
   {
     currentPose = odometry->getPose();
 
     if (elapsedTime < accelerationTime) // Accelerating
+    {
       // d = (a * t^2) / 2
-      setpoint = (motionProfileSettings.maximumAcceleration * pow(elapsedTime, 2)) / 2;
+      setpoint = (motionProfileSettings.maximumAcceleration * pow(elapsedTime, 2)) / (double)2;
+
+      setpointVelocity = motionProfileSettings.maximumAcceleration * elapsedTime;
+      setpointAcceleration = motionProfileSettings.maximumAcceleration;
+    }
     else if (elapsedTime < accelerationTime + cruiseTime) // Cruising
     {
       // How long we've been cruising for
       double elapsedCruiseTime = elapsedTime - accelerationTime;
-      setpoint = accelerationDistance + (maximumReachableVelocity * elapsedCruiseTime);
+      setpoint = (accelerationDistance + (maximumReachableVelocity * elapsedCruiseTime));
+
+      setpointVelocity = maximumReachableVelocity;
+      setpointAcceleration = 0;
     }
     else // Decelerating
     {
@@ -56,23 +75,27 @@ Chassis::trapezoidalMotionProfile(double distance, MotionProfile motionProfileSe
       double travelledDecelerationDistance = maximumReachableVelocity * elapsedDecelerationTime - (motionProfileSettings.maximumAcceleration * pow(elapsedDecelerationTime, 2)) / 2;
 
       setpoint = accelerationDistance + cruiseDistance + travelledDecelerationDistance;
+
+      setpointVelocity = maximumReachableVelocity - (motionProfileSettings.maximumAcceleration * elapsedDecelerationTime);
+      setpointAcceleration = motionProfileSettings.maximumAcceleration;
     }
 
-    feedforward = kV * setpoint + motionProfileSettings.kA * motionProfileSettings.maximumAcceleration;
+    feedforward = kV * setpointVelocity + motionProfileSettings.kA * setpointAcceleration;
 
     driveError = setpoint - (startPose.position.distanceTo(currentPose.position));
-    turnError = currentPose.orientation.angleTo(startPose.orientation);
+    turnError = currentPose.orientation.angleTo(startPose.orientation).angle;
 
     driveOutput = driveFeedbackPID.compute(driveError);
     turnOutput = turnFeedbackPID.compute(turnError);
 
     Pair motorOutputs = getMotorVelocities(driveOutput, turnOutput);
-    Left.spin(fwd, motorOutputs.left + feedforward, volt);
-    Right.spin(fwd, motorOutputs.right + feedforward, volt);
+    Left.spin(fwd, feedforward + motorOutputs.left, volt);
+    Right.spin(fwd, feedforward + motorOutputs.right, volt);
 
-    elapsedTime += (settings.updateTime / 1000); // Convert the update time from millseconds to seconds
+    elapsedTime += (settings.updateTime / (double)1000); // Convert the update time from millseconds to seconds
     wait(settings.updateTime, msec);
   }
 
-  double totalTime = 0;
+  Left.stop(brake);
+  Right.stop(brake);
 }
