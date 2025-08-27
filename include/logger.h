8ec35e7bd514688ecc2a64357ba58c8e3d649a93
Github11200 <78944617+Github11200@users.noreply.github.com>
2025-08-26 20:23:41 -0700

    logger changes

--- include/logger.h
+++ include/logger.h
@@ -2,34 +2,20 @@
 #define LOGGER_H
 
 #include "vex.h"
-#include "types/pose.h"
 
 namespace Logger
 {
 
-  enum MessageType
+  enum LogType
   {
-    POSITION = 0,
-    MOTION_START = 1,
-    MOTION_END = 2,
-    MESSAGE = 3
+    INFO = 0,
+    WARN = 1,
+    ERROR = 2
   };
 
-  enum MotionType
-  {
-    DRIVE_TO_POINT = 0,
-    DRIVE_TO_POSE = 1,
-    TURN_TO = 2,
-    PURSUIT = 3
-  };
+  std::string getPrefix(LogType logType);
 
-  std::string messageTypeEnumToString(MessageType messageType);
-  std::string motionTypeEnumToString(MotionType motionType);
-
-  void sendPositionData(Pose<double> &currentPose);
-  void sendMotionStart(MotionType motionType, Pose<double> target, std::string extraParams);
-  void sendMotionEnd(double elapsedTime);
-  void sendMessage(std::string message);
+  void print(LogType type, std::string message);
 
 };
 
