860f73630c083ca8b1647a6e7feec26f0b9c2b55
Github11200 <78944617+Github11200@users.noreply.github.com>
2025-09-23 22:15:30 -0700

    added in a class for autons

--- include/autons.h
+++ include/autons.h
@@ -3,12 +3,25 @@
 
 #include "vex.h"
 
+enum AutonName
+{
+  TESTING = 0
+};
+
+void testing();
+void left();
+void right();
+
 class Autons
 {
 private:
-  std::string allianceColor;
+  vex::color allianceColor;
+  std::function<void(void)> autonToRun;
 
 public:
+  void printMessage(std::string message);
+  void prepareAuton(AutonName name, vex::color allianceColor);
+  void runAuton();
 };
 
 #endif
